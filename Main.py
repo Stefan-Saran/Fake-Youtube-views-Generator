85dce4824d9db5766044b55cf3775fdfaabb458e
Stefan Saran <57372072+Stefan-Saran@users.noreply.github.com>
2020-05-27 17:58:44 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -15,7 +15,8 @@ import os
 import signal
 
 
-App1 = tk.Tk()
+Generator = tk.Tk()
+Generator.title("Fake Youtube Views Generator")
 
 CHROME_PATH = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
 CHROMEDRIVER_PATH = 'chromedriver\chromedriver.exe'
@@ -27,69 +28,65 @@ chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
 chrome_options.binary_location = CHROME_PATH
 
 
-def popup_bonus():
-    root = Toplevel(background="#1A1A1A")
+def Programm_info():
+    Popup_info = Toplevel(background="#1A1A1A")
     large_font2 = ('Verdana', 12)
-    root.title("Commands")
-    w = 300     # popup window width
-    h = 400     # popup window height
-    sw = root.winfo_screenwidth()
-    sh = root.winfo_screenheight()
-    x = (sw - w)/2
-    y = (sh - h)/2
-    root.geometry('%dx%d+%d+%d' % (w, h, x, y))
-    m = """
-
+    Popup_info.title("Commands")
+    width = 300
+    height = 400
+    screen_width = Popup_info.winfo_screenwidth()
+    screen_height = Popup_info.winfo_screenheight()
+    x = (screen_width - width)/2
+    y = (screen_height - height)/2
+    Popup_info.geometry('%dx%d+%d+%d' % (width, height, x, y))
+    About_info = """
 |This is Fake Youtube\n views generator|
 ---------------------------
-
 This program refreshes \n a certain video \n link on Youtube and that \n creates fake views on the video.
-
 ---------------------------
 Creator = Stefan Saran
 ---------------------------
-
 ---------------------------
 Creation date: 
 May 1st. 2020 - May 5th. 2020
 ---------------------------"""
-    m += '\n'
-    w = Label(root, text=m, width=120, height=20,
-              background="#1A1A1A", foreground="white", font=large_font2)
-    w.pack()
+    About_info += '\n'
+    about_label = Label(Popup_info, text=About_info, width=120, height=20,
+                        background="#1A1A1A", foreground="white", font=large_font2)
+    about_label.pack()
 
-    root.transient(App1)
-    root.grab_set()
-    root.wait_window(root)
+    Popup_info.transient(Generator)
+    Popup_info.grab_set()
+    Popup_info.wait_window(Popup_info)
     try:
-        root.resizable(False, False)
+        Popup_info.resizable(False, False)
     except TclError:
         pass
-    root.mainloop()
+    Popup_info.mainloop()
 
 
-def center_window(width, height):
-    screen_width = App1.winfo_screenwidth()
-    screen_height = App1.winfo_screenheight()
+def centering_window(width, height):
+    screen_width = Generator.winfo_screenwidth()
+    screen_height = Generator.winfo_screenheight()
 
     # calculate position x and y coordinates
     x = (screen_width/2) - (width/2)
     y = (screen_height/2) - (height/2)
-    App1.geometry('%dx%d+%d+%d' % (width, height, x, y))
+    Generator.geometry('%dx%d+%d+%d' % (width, height, x, y))
 
 
-def popup_bonus2():
-    root = Toplevel(background="#1A1A1A")
-    large_font2 = ('Verdana', 12)
-    root.title("Commands")
-    w = 300     # popup window width
-    h = 400     # popup window height
-    sw = root.winfo_screenwidth()
-    sh = root.winfo_screenheight()
-    x = (sw - w)/2
-    y = (sh - h)/2
-    root.geometry('%dx%d+%d+%d' % (w, h, x, y))
-    m = """
+def Programm_help():
+    Popup_help = Toplevel(background="#1A1A1A")
+    font_help = ('Verdana', 12)
+    Popup_help.title("Commands")
+    popup_width = 300     # popup window width
+    popup_height = 400     # popup window height
+    screen_width = Popup_help.winfo_screenwidth()
+    screen_height = Popup_help.winfo_screenheight()
+    x = (screen_width - popup_width)/2
+    y = (screen_height - popup_height)/2
+    Popup_help.geometry('%dx%d+%d+%d' % (popup_width, popup_height, x, y))
+    Help_info = """
 -----------------------------------------
 Entering Minimum 30 seconds\nafter every refresh, youtube \nwill count it as a view.\n
 Entering below 30 seconds, youtube\nwill mostly remove fake views.
@@ -99,84 +96,84 @@ Fake views will need time\nto be updated on youtube video.
 Only Chrome web \nbrowser supported!
 -----------------------------------------
 """
-    m += '\n'
-    w = Label(root, text=m, width=120, height=20,
-              background="#1A1A1A", foreground="white", font=large_font2)
-    w.pack()
+    Help_info += '\n'
+    popup_width = Label(Popup_help, text=Help_info, width=120, height=20,
+                        background="#1A1A1A", foreground="white", font=font_help)
+    popup_width.pack()
 
-    root.transient(App1)
-    root.grab_set()
-    root.wait_window(root)
+    Popup_help.transient(Generator)
+    Popup_help.grab_set()
+    Popup_help.wait_window(Popup_help)
     try:
-        root.resizable(False, False)
+        Popup_help.resizable(False, False)
     except TclError:
         pass
-    root.mainloop()
+    Popup_help.mainloop()
 
 
 refreshes = IntVar()
 refreshes.set(0)
 
 
+def error_popup():
+    error_ocurred = tk.messagebox.showerror(
+        "Error", "Random error ocurred")
+    if error_ocurred:
+        Generator.destroy()
+
+
 def website():
-    browser = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH,
-                               chrome_options=chrome_options)
-    browser.service.process  # is a Popen instance for the chromedriver process
-    p = psutil.Process(browser.service.process.pid)
-    for i in itertools.count():
-        browser.get(youtube_entry.get())
-        update1 = refreshes.get() + 1
-        refreshes.set(update1)
-        sleep(int(video_duration.get()))
-        # a = browser.find_element_by_class_name(
-        # 'style-scope ytd-video-primary-info-renderer').text
-        #views = ""
-        #views += a
-        #slice1 = views[20:23].strip()
-        # print(slice1)
+    try:
+        browser = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH,
+                                   chrome_options=chrome_options)
+        browser.service.process
+        p = psutil.Process(browser.service.process.pid)
+        for i in itertools.count():
+            browser.get(youtube_entry.get())
+            update1 = refreshes.get() + 1
+            refreshes.set(update1)
+            sleep(int(video_duration.get()))
+    except:
+        error_popup()
 
 
-class task_1(Thread):
+class Refresh_window(Thread):
     def run(self):
-        appp = tk.Toplevel(background="#1A1A1A")
-        appp.title("Refreshes")
+        refresh_counter = tk.Toplevel(background="#1A1A1A")
+        refresh_counter.title("Refreshes")
         refresh_label = tk.Label(
-            appp, width=30, height=3, bg="white", textvariable=str(refreshes))
+            refresh_counter, width=30, height=3, bg="white", textvariable=str(refreshes))
         refresh_label.place(rely=0.4, relx=0.5, anchor="center")
-        appp.geometry("220x100")
-        appp_button = tk.Button(
-            appp, width=10, text="Stop", command=lambda: exit(self))
-        appp_button.pack(side="bottom")
-        appp.resizable(False, False)
-        center_window(220, 100)
-        appp.protocol("WM_DELETE_WINDOW", App1.destroy)
-        appp.mainloop()
+        refresh_counter.geometry("220x100")
+        refresh_button = tk.Button(
+            refresh_counter, width=10, text="Stop", command=lambda: exit(self))
+        refresh_button.pack(side="bottom")
+        refresh_counter.resizable(False, False)
+        refresh_counter.protocol("WM_DELETE_WINDOW", Generator.destroy)
 
-    def exit(self):
-        App1.destroy()
-
-    def center_window(self, width, height):
-        self.screen_width = App1.winfo_screenwidth()
-        self.screen_height = App1.winfo_screenheight()
+        self.screen_width = Generator.winfo_screenwidth()
+        self.screen_height = Generator.winfo_screenheight()
 
         # calculate position x and y coordinates
-        self.x = (screen_width/2) - (width/2)
-        self.y = (screen_height/2) - (height/2)
-        run.appp.geometry('%dx%d+%d+%d' % (width, height, x, y))
+        self.x = (self.screen_width/2) - (220/2)
+        self.y = (self.screen_height/2) - (100/2)
+        refresh_counter.geometry('%dx%d+%d+%d' % (220, 100, self.x, self.y))
+
+        refresh_counter.mainloop()
+
+    def exit(self):
+        Generator.destroy()
 
 
-class task_2(Thread):
+class running_website(Thread):
     def run(self):
         website()
 
 
-def callback():
-    App1.withdraw()
-    task_1().start()
-    task_2().start()
-
-
-test = False
+def running_at_the_same_time():
+    Generator.withdraw()
+    Refresh_window().start()
+    running_website().start()
 
 
 def special():
@@ -202,49 +199,49 @@ def warnings():
         tk.messagebox.showerror(
             "Error", "The limit is 60 seconds after every refresh!")
     else:
-        callback()
+        running_at_the_same_time()
 
 
-def add1(add1):
+def delete_youtube_entry_text(add1):
     if youtube_entry.get() == "Insert the YouTube video link here":
         youtube_entry.delete(0, END)
     else:
         pass
 
 
-def add2(add2):
+def delete_video_duration_text(add22):
     if video_duration.get() == "seconds after every refresh (max 60)":
         video_duration.delete(0, END)
     else:
         pass
 
 
-about_button = Button(App1, text="About", width=10, command=popup_bonus)
+about_button = Button(Generator, text="About", width=10, command=Programm_info)
 about_button.place(rely=0.03, relx=0.03)
 
-start_button = Button(App1, text="Start", width=10, command=warnings)
+start_button = Button(Generator, text="Start", width=10, command=warnings)
 start_button.place(rely=0.8, relx=0.4)
 
-help_button = Button(App1, text="Help", width=10, command=popup_bonus2)
+help_button = Button(Generator, text="Help", width=10, command=Programm_help)
 help_button.place(rely=0.03, relx=0.8)
 
-App1.configure(bg="#1A1A1A")
+Generator.configure(bg="#1A1A1A")
 
-center_window(500, 300)
+centering_window(500, 300)
 
 large_font = ('Verdana', 17)
-youtube_entry = tk.Entry(App1, font=large_font, width=28)
+youtube_entry = tk.Entry(Generator, font=large_font, width=28)
 youtube_entry.place(rely=0.4, relx=0.5, anchor="center")
 youtube_entry.configure(foreground="gray")
 
 
-video_duration = tk.Entry(App1, width=35)
+video_duration = tk.Entry(Generator, width=35)
 video_duration.place(rely=0.55, relx=0.5, anchor="center")
 video_duration.insert(0, "seconds after every refresh (max 60)")
 youtube_entry.insert(0, "Insert the YouTube video link here")
-video_duration.bind("<Button-1>", add2)
-youtube_entry.bind("<Button-1>", add1)
+video_duration.bind("<Button-1>", delete_video_duration_text)
+youtube_entry.bind("<Button-1>", delete_youtube_entry_text)
 video_duration.configure(foreground="gray")
 
-App1.resizable(False, False)
-App1.mainloop()
+Generator.resizable(False, False)
+Generator.mainloop()
