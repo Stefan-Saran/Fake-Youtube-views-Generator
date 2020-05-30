3fd286856aaa2baac5eedb9b21678dede5ee3ec0
Stefan Saran <57372072+Stefan-Saran@users.noreply.github.com>
2020-05-30 17:23:42 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -28,8 +28,8 @@ WINDOW_SIZE = "1920, 1080"
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
+chrome_options.add_argument("--mute-audio")
 chrome_options.binary_location = CHROME_PATH
-
 
 def Programm_info():
     Popup_info = Toplevel(background="#1A1A1A")
